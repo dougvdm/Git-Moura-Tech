c1ac895035f66881640998910623ccf0b5e698b8
dougvdm <douglasvianademacedo2002@gmail.com>
2026-05-20 21:08:26 -0300

    teste finalizado

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //'https://api.github.com/users/gleydsonbrito/repos'
 
-const input = document.querySelector('input-text')
+const input = document.querySelector('.input-text')
 const ul = document.querySelector('.repos')
 
-const allRepositories = []
+let allRepositories = []
 
 function render(repos, filter){
         ul.innerHTML = ""
@@ -15,11 +15,21 @@ function render(repos, filter){
             return true 
         } )
         filteredRepos.forEach(repo => {
-            const li = document.querySelector("li")
+            const li = document.createElement("li")
             const textItem = document.createTextNode(repo.name)
             li.appendChild(textItem)
             ul.appendChild(li)
         });
 }
+input.addEventListener('input', event  => {
+    const text = event.target.value
+    render(allRepositories, text)
+})
 
 
+fetch('https://api.github.com/users/dougvdm/repos')
+.then(res => res.json())
+.then(repos => {
+    allRepositories = repos
+    render(allRepositories, '')
+})
